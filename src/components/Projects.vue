b57cdc5aa28e6fbbd3fb89a5e3601e1b8d3a954c
lunaerem <140562305+lunaerem@users.noreply.github.com>
2026-03-06 13:58:03 -0600

    Large progress on Project page layout

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -7,16 +7,23 @@
 	<p>Some of the various projects that I have worked on:</p>	
 	<div id="wrapper">
 	    <div class="project">
-		#1
+		<img src="/vite.svg" alt="Vite logo" class="image" />
+		<div class="project-info">
+		    <div class="project-text">
+			<h3 class="title-underscore">Personal Website</h3>
+			<p>The website you are viewing now! My personal website made using Vue and Vite.</p>
+		    </div>
+		    <div class="project-stats">
+			<div>
+			    Chips
+			</div>
+			<button class="nav-button">Go to Project</button>
+		    </div>
+		</div>
 	    </div>
 	    <div class="project">
-		#2
 	    </div>
 	    <div class="project">
-		#3
-	    </div>
-	    <div class="project">
-		#4
 	    </div>
 	</div>
 	<div id="pag-header">
@@ -31,13 +38,38 @@
 #wrapper {
     flex-grow: 1; 
     display: grid;
-    min-height: 50%;
     gap: 1em;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    margin-bottom: 1em;
 }
 
 .project {
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+
+    img {
+	width: 100%;
+    }
+}
+
+.project-info {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+}
+
+.project-text {
+    flex-grow: 1;
+}
+
+.project-stats {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 #pag-header {
