d67fd1ee3997f76abd93b158d61b75dec8622fbc
lunaerem <140562305+lunaerem@users.noreply.github.com>
2026-07-31 17:42:52 -0500

    Update projects list

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,102 +1,120 @@
 <script setup lang="ts">
-const props = defineProps<{ title?: string, text?: string, src?: string, alt?: string, chips?: string[], link?: string, source?: string }>()
+const props = defineProps<{
+  title?: string;
+  text?: string;
+  src?: string;
+  alt?: string;
+  chips?: string[];
+  link?: string;
+  source?: string;
+}>();
 </script>
 
 <template>
-    <img :src="src" :alt="alt" />
-    <div class="project-info">
-	<div class="project-text">
-	    <h3 class="title-underscore">{{ title }}</h3>
-	    <p>{{ text }}</p>
-	</div>
-	<div class="project-stats">
-	    <div id="chip-container">
-		<div v-for="(chip, index) in props.chips" class="chip" :class="chip">{{ chips![index] }}</div>
-	    </div>
-	    <div id="project-lnk-container">
-		<a v-if="link != null" class="nav-button" target="_blank" :href="link">Project</a>
-		<a class="nav-button" target="_blank" :href="source">Source</a>
-	    </div>
-	</div>
+  <img :src="src" :alt="alt" />
+  <div class="project-info">
+    <div class="project-text">
+      <h3 class="title-underscore">{{ title }}</h3>
+      <p>{{ text }}</p>
     </div>
+    <div class="project-stats">
+      <div id="chip-container">
+        <div v-for="(chip, index) in props.chips" class="chip" :class="chip">
+          {{ chips![index] }}
+        </div>
+      </div>
+      <div id="project-lnk-container">
+        <a v-if="link != null" class="nav-button" target="_blank" :href="link"
+          >Project</a
+        >
+        <a
+          v-if="source != null"
+          class="nav-button"
+          target="_blank"
+          :href="source"
+          >Source</a
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 img {
-    max-width: 100%;
+  max-width: 100%;
 }
 
 .project-info {
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
 }
 
 .project-text {
-    flex-grow: 1;
+  flex-grow: 1;
 }
 
 .project-stats {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 6px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
 }
 
 #project-lnk-container {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 #chip-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 4px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
 }
 
 .chip {
-    background-color: #4a5568;
-    border-radius: 20px;
-    border: 0.15rem solid rgba(255, 255, 255, 1);
-    padding: 0.5rem 1rem;
-    color: white;
-    box-shadow: 0 8px 32px rgba(31, 38, 135, 0.5);
+  background-color: #4a5568;
+  border-radius: 20px;
+  border: 0.15rem solid rgba(255, 255, 255, 1);
+  padding: 0.5rem 1rem;
+  color: white;
+  box-shadow: 0 8px 32px rgba(31, 38, 135, 0.5);
 }
 
 .HTML {
-    background-color: rgba(255, 0, 0, 0.75);
+  background-color: rgba(255, 0, 0, 0.75);
 }
 
 .CSS {
-    background-color: rgba(0, 0, 255, 0.75);
+  background-color: rgba(0, 0, 255, 0.75);
 }
 
 .TypeScript {
-    background-color: rgba(255, 255, 0, 0.75);
-    color: black;
+  background-color: rgba(255, 255, 0, 0.75);
+  color: black;
 }
 
 .Vue {
-    background-color: rgba(0, 255, 0, 0.75);
+  background-color: rgba(0, 255, 0, 0.75);
 }
 
 .C {
-    background-color: rgba(0, 255, 255, 0.75);
-    color: black;
+  background-color: rgba(0, 255, 255, 0.75);
+  color: black;
 }
 
 .Cpp {
-    background-color: rgba(0, 0, 0, 0.75);
+  background-color: rgba(0, 0, 0, 0.75);
 }
 
 .QT {
-    background-color: rgba(255, 192, 203, 0.75);
-    color: black;
+  background-color: rgba(255, 192, 203, 0.75);
+  color: black;
 }
 
 @media (orientation: portrait) {
-    #project-lnk-container {
-	flex-direction: column;
-    }
+  #project-lnk-container {
+    flex-direction: column;
+  }
 }
 </style>
