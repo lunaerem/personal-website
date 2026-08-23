a6bc7dd89b3bdca7cf0fa81402a5158a1d155816
lunaerem <140562305+lunaerem@users.noreply.github.com>
2026-08-23 16:40:31 -0500

    Fixed some highlighting inconsistencies

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,71 +1,80 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="flex-wrapper"> 
-	<h2>This is my personal website!</h2>
-	<p style="margin-top: 0">In short, I am a programmer, developer, modder, musician, and
-	organisational enthusiast, but feel free to take a look around to
-	get a deeper understanding of who I am and the kinds of projects
-	I work on!</p>
-	<nav id="wrapper">
-	    <RouterLink to="/about">
-		<img alt="About Me Button Icon" src="../assets/About.svg" />
-		About Me
-	    </RouterLink>
-	    <RouterLink to="/projects">
-		<img alt="My Projects Button Icon" src="../assets/Projects.svg" />
-		My Projects
-	    </RouterLink>
-	    <RouterLink to="/credits">
-		<img alt="Credits Button Icon" src="../assets/Credits.svg" />
-		Credits
-	    </RouterLink>
-	    <a target="_blank" href="https://github.com/lunaerem">
-		<img alt="My GitHub Button Icon" src="../assets/Git.svg" />
-		My GitHub
-	    </a>
-	</nav>
-    </div>
+  <div class="flex-wrapper">
+    <h2>This is my personal website!</h2>
+    <p style="margin-top: 0">
+      In short, I am a programmer, developer, modder, musician, and
+      organisational enthusiast, but feel free to take a look around to get a
+      deeper understanding of who I am and the kinds of projects I work on!
+    </p>
+    <nav id="wrapper">
+      <RouterLink to="/about">
+        <img alt="About Me Button Icon" src="../assets/About.svg" />
+        About Me
+      </RouterLink>
+      <RouterLink to="/projects">
+        <img alt="My Projects Button Icon" src="../assets/Projects.svg" />
+        My Projects
+      </RouterLink>
+      <RouterLink to="/credits">
+        <img alt="Credits Button Icon" src="../assets/Credits.svg" />
+        Credits
+      </RouterLink>
+      <a target="_blank" href="https://github.com/lunaerem">
+        <img alt="My GitHub Button Icon" src="../assets/Git.svg" />
+        My GitHub
+      </a>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
 #wrapper {
-    flex-grow: 1; 
-    display: grid;
-    gap: 1em;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    
-    a {
-	--hue: 220;
-	--button-background: oklch(75% 0.1 var(--hue) / 0.8);
-	--bg-dark: oklch(15% 0.1 var(--hue) / 0.75);
-	--button-foreground: oklch(15% 0.05 var(--hue));
+  flex-grow: 1;
+  display: grid;
+  gap: 1em;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
 
-	cursor: pointer;
-	background: transparent;
-	border-radius: 10px;
-	background: linear-gradient(to bottom, rgba(255, 255, 255, 0.55), var(--button-background) 40%, var(--bg-dark));
-	border: 3px solid var(--button-background);
-	transition: box-shadow 0.25s;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+  a {
+    --hue: 220;
+    --button-background: oklch(75% 0.1 var(--hue) / 0.8);
+    --bg-dark: oklch(15% 0.1 var(--hue) / 0.75);
+    --button-foreground: oklch(15% 0.05 var(--hue));
 
-	&:hover {
-	    box-shadow: inset 0 0 20px #2dab96, 0 0 10px 2px #ffffffbf;
-	}
+    cursor: pointer;
+    background: transparent;
+    border-radius: 10px;
+    background: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.55),
+      var(--button-background) 40%,
+      var(--bg-dark)
+    );
+    border: 3px solid var(--button-background);
+    transition: box-shadow 0.25s;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    user-select: none;
+    -webkit-user-select: none;
 
-	&:active {
-	    transform: translateY(1px);
-	}
+    &:hover {
+      box-shadow:
+        inset 0 0 20px #2dab96,
+        0 0 10px 2px #ffffffbf;
     }
 
-    img {
-	min-width: 30%;
-	max-width: 40%;
+    &:active {
+      transform: translateY(1px);
     }
+  }
+
+  img {
+    min-width: 30%;
+    max-width: 40%;
+  }
 }
 </style>
